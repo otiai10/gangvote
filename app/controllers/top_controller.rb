ce17e44bca37308a62425f7e1b2dbe78d0d894ca
otiai10 <otiai10@gmail.com>
2013-03-09 15:49:57 +0900

    ゲームにログインして、ホームチームとアウェイチームとべつにリスト閲覧できるようにした

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,5 +3,12 @@ class TopController < ApplicationController
     @user_name = cookies[:user_name]
     @games = Game.all
     @game  = Game.last
+    @game_join  = session[:game]
+
+    @vote_left = cookies[:vote_left]
+    if @vote_left.nil?
+      @vote_left = 0
+    end
+
   end
 end
